cd5c23a3cf7bc5762869faec2d8af0e91f6f06e2
Tommy Roscio <tommyroscio6@gmail.com>
2025-11-24 14:22:34 +0000

    Adds player movement with boundaries

--- MyFirstRaylib/main.cpp
+++ MyFirstRaylib/main.cpp
@@ -1,19 +1,54 @@
 #include "raylib.h"
 
 int main()
-{
-	const int windowWidth = 1600;
-	const int windowHeight = 1200;
+{	
+	int playerX = 350;
+	int playerY = 250;
+	const int playerRectangleWidth = 250;
+	const int playerRectangleHeight = 100;
+	const int windowWidth = 800;
+	const int windowHeigth = 600;
 
-	InitWindow(windowWidth, windowHeight, "Success");
-	SetTargetFPS(165);
+	InitWindow(windowWidth, windowHeigth, "Success");
+	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
-	{
+	{	
+		// Movement
+		if (IsKeyDown(KEY_RIGHT)) {
+			playerX += 5;
+		}
+		if (IsKeyDown(KEY_LEFT)) {
+			playerX -= 5;
+		}
+		if (IsKeyDown(KEY_UP)) {
+			playerY -= 5;
+		}
+		if (IsKeyDown(KEY_DOWN)) {
+			playerY += 5;
+		}
+
+		// Boundaries Check
+		if (playerX < 0) {
+			playerX = 0;
+		}
+
+		if (playerX + playerRectangleWidth > windowWidth) {
+			playerX = windowWidth - playerRectangleWidth;
+		}
+
+		if (playerY < 0) {
+			playerY = 0;
+		}
+
+		if (playerY + playerRectangleHeight > windowHeigth) {
+			playerY = windowHeigth - playerRectangleHeight;
+		}
+
+		// Drawing Logic
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText("System Online: Hello World", 190, 200, 50, LIGHTGRAY);
-		DrawRectangle(windowWidth / 2 - 25, windowHeight / 2 - 25, 50, 50, RED);
+		DrawRectangle(playerX, playerY, playerRectangleWidth, playerRectangleHeight, RED);
 		EndDrawing();
 	}
 
